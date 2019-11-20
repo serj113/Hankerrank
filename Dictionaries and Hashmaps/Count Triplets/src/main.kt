5d039b30a64b28a48819dd0f987b921fa2205911
Setia <setia_simaremare@yahoo.com>
2019-11-20 16:07:36 +0700

    move to related topic and added count triplet

---
+++ Dictionaries and Hashmaps/Count Triplets/src/main.kt
@@ -0,0 +1,42 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the countTriplets function below.
+fun countTriplets(arr: Array<Long>, r: Long): Long {
+    val map1 = hashMapOf<Long, Long>()
+    val map2 = hashMapOf<Long, Long>()
+    val starter = arr[0]
+    for (i in 1 until arr.count()) {
+
+    }
+}
+
+fun main(args: Array<String>) {
+    val nr = readLine()!!.trimEnd().split(" ")
+
+    val n = nr[0].toInt()
+
+    val r = nr[1].toLong()
+
+    val arr = readLine()!!.trimEnd().split(" ").map{ it.toLong() }.toTypedArray()
+
+    val ans = countTriplets(arr, r)
+
+    println(ans)
+}
