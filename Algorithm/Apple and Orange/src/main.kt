ee817aad509f3833b42d04561745c4a2aec2006e
Setia <setia_simaremare@yahoo.com>
2020-08-04 17:45:22 +0700

    added grading students, and apple and orange

---
+++ Algorithm/Apple and Orange/src/main.kt
@@ -0,0 +1,66 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the countApplesAndOranges function below.
+fun countApplesAndOranges(s: Int, t: Int, a: Int, b: Int, apples: Array<Int>, oranges: Array<Int>): Unit {
+    var totalApple = 0
+    var totalOrange = 0
+    apples.forEach {
+        val value = it + a
+        if (value in s..t) {
+            totalApple += 1
+        }
+    }
+    oranges.forEach {
+        val value = it + b
+        if (value in s..t) {
+            totalOrange += 1
+        }
+    }
+    println(totalApple)
+    println(totalOrange)
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val st = scan.nextLine().split(" ")
+
+    val s = st[0].trim().toInt()
+
+    val t = st[1].trim().toInt()
+
+    val ab = scan.nextLine().split(" ")
+
+    val a = ab[0].trim().toInt()
+
+    val b = ab[1].trim().toInt()
+
+    val mn = scan.nextLine().split(" ")
+
+    val m = mn[0].trim().toInt()
+
+    val n = mn[1].trim().toInt()
+
+    val apples = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val oranges = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
+}
