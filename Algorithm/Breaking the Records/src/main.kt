d95c13ea33cade5b4c5a605ab25a4f8d6df57f01
Setia <setia_simaremare@yahoo.com>
2020-08-07 23:41:30 +0700

    added scratch

---
+++ Algorithm/Breaking the Records/src/main.kt
@@ -0,0 +1,36 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the breakingRecords function below.
+fun breakingRecords(scores: Array<Int>): Array<Int> {
+
+
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val scores = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = breakingRecords(scores)
+
+    println(result.joinToString(" "))
+}
---
+++ Dynamic Programming/Max Array Sum/src/main.kt
@@ -0,0 +1,40 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the maxSubsetSum function below.
+fun maxSubsetSum(arr: Array<Int>): Int {
+    var isEnd = false
+    val result = arrayListOf<Int>()
+    var current = 0
+    while (!isEnd) {
+
+    }
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val arr = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val res = maxSubsetSum(arr)
+
+    println(res)
+}
