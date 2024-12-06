3801ab233d9940de98165b627e0857e78d6bf5a4
Setia <putra.simaremare@gmail.com>
2024-12-06 22:20:21 +0700

    added lonely integer

---
+++ Algorithm/Lonely Integer/src/Main.kt
@@ -0,0 +1,48 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+fun lonelyinteger(a: Array<Int>): Int {
+    val set = hashSetOf<Int>()
+    a.forEach { value ->
+        if (set.contains(value)) {
+            set.remove(value)
+        } else {
+            set.add(value)
+        }
+    }
+
+    return set.first()
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val a = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = lonelyinteger(a)
+
+    println(result)
+}
