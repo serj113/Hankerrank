ba0b3b2df11e90c04bbc922a1dc26658b49d288d
Setia <putra.simaremare@gmail.com>
2024-12-16 22:06:58 +0700

    added two sum

---
+++ Leetcode/L-1 Two Sum/src/Main.kt
@@ -0,0 +1,27 @@
+fun main() {
+    println(
+        Solution().twoSum(
+            nums = intArrayOf(
+                -3, 4, 3, 90
+            ),
+            target = 0
+        ).toString()
+    )
+}
+
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val map = hashMapOf<Int, Int>()
+        var result = intArrayOf(0, 0)
+        for (i in 0..nums.lastIndex) {
+            if (map.containsKey(nums[i])) {
+                result = intArrayOf(map[nums[i]] ?: 0, i)
+                break
+            } else {
+                map[target - nums[i]] = i
+            }
+        }
+
+        return result
+    }
+}
