dbf290995c0f840a449fb7c132d87d191e8a36cc
Setia <putra.simaremare@gmail.com>
2025-01-12 23:37:49 +0700

    added minimum depth of binary tree

---
+++ Leetcode/L-111 Minimum Depth of Binary Tree/src/Main.kt
@@ -0,0 +1,44 @@
+fun main() {
+    println(
+        Solution()
+            .minDepth(
+                root = TreeNode(
+                    `val` = 2,
+                    left = null,
+                    right = TreeNode(
+                        `val` = 3,
+                        left = null,
+                        right = TreeNode(
+                            `val` = 4,
+                            left = null,
+                            right = TreeNode(
+                                `val` = 5,
+                                left = null,
+                                right = TreeNode(
+                                    `val` = 6,
+                                ),
+                            ),
+                        )
+                    ),
+                )
+            )
+    )
+}
+
+class TreeNode(var `val`: Int, var left: TreeNode? = null, var right: TreeNode? = null)
+
+class Solution {
+    fun minDepth(root: TreeNode?): Int {
+        if (root == null) return 0
+        val value = if (root.left != null || root.right != null) {
+            val left = root.left?.let {
+                minDepth(it)
+            } ?: Int.MAX_VALUE
+            val right = root.right?.let {
+                minDepth(it)
+            } ?: Int.MAX_VALUE
+            Math.min(left, right)
+        } else 0
+        return 1 + value
+    }
+}
