5b829873b103db8d2767f88ecce0bb924a189136
Setia <putra.simaremare@gmail.com>
2025-02-06 20:57:20 +0700

    added range sum query

---
+++ Leetcode/L-303 Range Sum Query/src/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+}
+
+class NumArray(private val nums: IntArray) {
+
+    fun sumRange(left: Int, right: Int): Int {
+        var sum = 0
+        for (i in left..right) {
+            sum += nums[i]
+        }
+
+        return sum
+    }
+}
