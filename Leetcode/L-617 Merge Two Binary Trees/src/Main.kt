84da3384b89e6adb2a41aeb86719c204362baedc
Setia <putra.simaremare@gmail.com>
2025-02-24 22:59:48 +0700

    added merge two binary trees

---
+++ Leetcode/L-617 Merge Two Binary Trees/src/Main.kt
@@ -0,0 +1,22 @@
+fun main() {
+
+}
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+class Solution {
+    fun mergeTrees(root1: TreeNode?, root2: TreeNode?): TreeNode? {
+        if (root1 == null) return root2
+        if (root2 == null) return root1
+
+        root1.`val` += root2.`val`
+
+        root1.left = mergeTrees(root1.left, root2.left)
+        root1.right = mergeTrees(root1.right, root2.right)
+
+        return root1
+    }
+}
