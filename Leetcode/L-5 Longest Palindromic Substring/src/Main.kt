d721f9644d6432101ba8532749cbb2052c374b97
Setia <putra.simaremare@gmail.com>
2025-04-24 20:37:10 +0700

    updated longest palindrome

--- Leetcode/L-5 Longest Palindromic Substring/src/Main.kt
+++ Leetcode/L-5 Longest Palindromic Substring/src/Main.kt
@@ -1,32 +1,46 @@
-fun main() {
+import kotlin.math.max
 
+fun main() {
+    val x: MyClass<in Int> = MyClass<Number>()
+    val y: MyClass<out Any> = MyClass<String>()
+    val z: MyClass<out String> = MyClass<Any>()
+    println(
+        Solution().longestPalindrome("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
+    )
 }
+
+class MyClass<T>
 
 class Solution {
     fun longestPalindrome(s: String): String {
-        var result = ""
-        var i = 0
-        var j = s.lastIndex
-        val mid = j / 2.toDouble()
-        while (i < j) {
+        var start = 0
+        var end = 0
+        var longest = 0
 
+        for (i in 0..s.length) {
+            val first = expand(i, i, s)
+            val second = expand(i, i + 1, s)
+            val length = max(first, second)
+            if (length > longest) {
+                longest = length
+                start = i - ((length - 1) / 2)
+                end = i + (length / 2)
+            }
         }
+
+        return s.substring(start, end + 1)
     }
 
-    private fun isPalindrome(s: String, start: Int, end: Int): Boolean {
-        val length = start - end + 1
-        val mid = if (length % 2 == 1) {
-            length / 2
-        } else (length / 2) - 1
+    private fun expand(start: Int, end: Int, s: String): Int {
+        if (end > s.lastIndex) return 0
         var i = start
         var j = end
-        while (i < j) {
-            if (s[i] != s[j]) {
-                return false
-            }
-            i++
-            j++
+
+        while (i >= 0 && j <= s.lastIndex && s[i] == s[j]) {
+            i -= 1
+            j += 1
         }
-        return true
+
+        return j - i - 1
     }
 }
